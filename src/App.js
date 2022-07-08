8abea76020d79e7587eb007a586db555d6e5a50a
0ffpudding <jennaanderson00@gmail.com>
2022-07-07 20:02:51 -0500

    added spinner component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Header from './components/header'
+import SearchForm from './components/searchForm'
+import PokemonDetails from './components/pokemonDetails'
+import Spinner from './components/spinner'
 
 function App() {
+  const [pokemon, setPokemon] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('ninetails')
+  
+  useEffect(() => {
+    console.log('Running useEffect...')
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <SearchForm />
+      <PokemonDetails />
+      <Spinner />
+    </>
   );
 }
 
---
+++ src/components/spinner.js
@@ -0,0 +1,5 @@
+const Spinner = () => {
+    return <div className="loader" />
+  }
+  
+  export default Spinner
