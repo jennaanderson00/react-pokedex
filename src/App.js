f0a851334e2333286ab23ad8500188236e923b6e
0ffpudding <jennaanderson00@gmail.com>
2022-07-07 20:33:42 -0500

    pokemonDetails component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,38 @@ function App() {
   const [pokemon, setPokemon] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
-  const [searchTerm, setSearchTerm] = useState('ninetails')
+  const [searchTerm, setSearchTerm] = useState('ninetales')
   
   useEffect(() => {
     console.log('Running useEffect...')
   }, [])
-  
+
+  const getPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+      .then(res => res.json())
+      .then(json => setPokemon(json))
+      .catch(err => setError('Pokemon not found'))
+      .finally(() => setLoading(false))
+  }
+
+  const renderUI = () => {
+    if (loading) 
+      return <Spinner />
+    else if (error) 
+      return <p className='error'>{error}</p>
+    else if (pokemon) 
+      return <PokemonDetails />
+    else if (!searchTerm)
+      return <p>Search a pokemon to get started!</p>
+    else
+      return null
+  }
+
   return (
     <>
       <Header />
       <SearchForm />
-      <PokemonDetails />
-      <Spinner />
+      { renderUI() }
     </>
   );
 }
--- src/components/pokemonDetails.js
+++ src/components/pokemonDetails.js
@@ -1,5 +1,36 @@
-const PokemonDetails = () => {
-    return 'PokemonDetails'
+import Container from './container'
+
+const PokemonDetails = props => {
+    const {
+        id,
+        name,
+        moves,
+        sprites,
+        base_experience,
+        height,
+        weight
+    } = props.pokemon
+
+    return (
+        <Container className='results'>
+            <img
+                src={sprites.other['official-artwork'].front_default}
+                alt={name}
+                height="auto"
+                width="100%"
+            />
+            <h1>{id}: {name}</h1>
+            <ul>
+                <li>Base Experience: {base_experience}</li>
+                <li>Height: {height}</li>
+                <li>Weight: {weight}</li>
+            </ul>
+            <h2>Moves</h2>
+            <ul>
+                {moves.map(moveObj => <li key={moveObj.move.name}>{moveObj.move.name}</li>)}
+            </ul>
+        </Container>
+    )
   }
   
   export default PokemonDetails
